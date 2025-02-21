8873136c660bba962d94da862dad03869666be90
zhonik <98616263+zhonik@users.noreply.github.com>
2025-02-21 19:52:55 +0000

    Deploying to gh-pages from @ zhonik/blended-products@68823f04f6092a1e64fdbb32bd3f54fad0b332a3 🚀

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-import"./assets/styles-BK7AYJoX.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";o.defaults.baseURL="https://dummyjson.com/products/";async function r(){const s="category-list";try{return(await o.get(s)).data}catch(t){console.log(t)}}r();
+import"./assets/styles-BK7AYJoX.js";import{a as s}from"./assets/vendor-N5iQpiFS.js";s.defaults.baseURL="https://dummyjson.com/products/";async function n(){return(await s.get("category-list")).data}const r={categoriesList:document.querySelector(".categories")};function a(e){e.unshift("All");const t=e.map(o=>`<li class="categories__item">
+        <button class="categories__btn" type="button">${o}</button>
+      </li>`).join("");r.categoriesList.innerHTML=t}async function c(e){try{const t=await n();a(t)}catch(t){console.log(t)}}document.addEventListener("DOMContentLoaded",c);
 //# sourceMappingURL=index.js.map
