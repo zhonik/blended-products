32029e4fff316b3e0a922356308bcdd371d7b160
Grygoriy Chernish <cga.jsdev@gmail.com>
2025-02-21 21:51:40 +0200

    render-categories

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -5,11 +5,12 @@ import { refs } from './refs';
 export function renderCategoriesList(categories) {
   categories.unshift('All');
   const markup = categories
-    .map(category => {
-      `<li class="categories__item">
+    .map(
+      category =>
+        `<li class="categories__item">
         <button class="categories__btn" type="button">${category}</button>
-      </li>`;
-    })
+      </li>`
+    )
     .join('');
 
   refs.categoriesList.innerHTML = markup;
