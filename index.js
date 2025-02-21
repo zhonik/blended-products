3e5aefe8379d2fe75233ca5cb82166f97233c9ef
zhonik <98616263+zhonik@users.noreply.github.com>
2025-02-21 19:06:03 +0000

    Deploying to gh-pages from @ zhonik/blended-products@af2d91d8fcf09b0d22f859c442ed99e9799fc440 🚀

---
+++ index.js
@@ -0,0 +1,2 @@
+import"./assets/styles-BK7AYJoX.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";o.defaults.baseURL="https://dummyjson.com/products/";async function r(){const s="category-list";try{return(await o.get(s)).data}catch(t){console.log(t)}}r();
+//# sourceMappingURL=index.js.map
