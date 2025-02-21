b4608e7a14d3d525a7adbadc990682ba80fc9348
Lena <stlena113@gmail.com>
2025-02-21 22:22:41 +0200

    products

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,13 @@
 // Функції для роботи з бекендом
+
+
+async function fetchProducts(page = 1) {
+    const skip = (page - 1) * 12;
+    const url = `?limit=12&skip=${skip}`;
+
+    
+        const response = await axios.get(url);
+        return response.data.products;
+
+
+}
