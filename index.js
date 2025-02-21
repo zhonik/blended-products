2e1e7abff41c9c03b5bfb734065149134bda9b43
zhonik <98616263+zhonik@users.noreply.github.com>
2025-02-21 19:56:37 +0000

    Deploying to gh-pages from @ zhonik/blended-products@2b0f6fd440543a495cc2404e57eb6104393990a6 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import"./assets/styles-BK7AYJoX.js";import{a as s}from"./assets/vendor-N5iQpiFS.js";s.defaults.baseURL="https://dummyjson.com/products/";async function n(){return(await s.get("category-list")).data}const r={categoriesList:document.querySelector(".categories")};function a(e){e.unshift("All");const t=e.map(o=>`<li class="categories__item">
+import"./assets/styles-BK7AYJoX.js";import{a as s}from"./assets/vendor-N5iQpiFS.js";s.defaults.baseURL="https://dummyjson.com/products/";async function n(){return(await s.get("category-list")).data}const r={productsList:document.querySelector(".products"),categoriesList:document.querySelector(".categories")};function c(e){e.unshift("all");const t=e.map(o=>`<li class="categories__item">
         <button class="categories__btn" type="button">${o}</button>
-      </li>`).join("");r.categoriesList.innerHTML=t}async function c(e){try{const t=await n();a(t)}catch(t){console.log(t)}}document.addEventListener("DOMContentLoaded",c);
+      </li>`).join("");r.categoriesList.innerHTML=t}async function a(e){try{const t=await n();c(t)}catch(t){console.log(t)}}document.addEventListener("DOMContentLoaded",a);
 //# sourceMappingURL=index.js.map
