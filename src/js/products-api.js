b0f142eac67dad4d029cd6388035e4974253d6b2
Oleh <therkasoa@gmail.com>
2025-02-21 20:59:07 +0200

    get categories list

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,13 @@
 // Функції для роботи з бекендом
+import axios from 'axios';
+export async function getProductCategories() {
+  const BASE_URL = 'https://dummyjson.com/products';
+  const END_POINT = '/category-list';
+  const url = `${BASE_URL}${END_POINT}`;
+  try {
+    const resp = await axios.get(url);
+    return resp.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
