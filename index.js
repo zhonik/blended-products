ed1363677761661a7a77cd3da200ed55d9392406
zhonik <98616263+zhonik@users.noreply.github.com>
2025-02-21 20:47:47 +0000

    Deploying to gh-pages from @ zhonik/blended-products@78bfd04658899aad3103bf0d1ab751cb14a40d51 🚀

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import"./assets/styles-BK7AYJoX.js";import{a as n}from"./assets/vendor-N5iQpiFS.js";const c={categoriesList:document.querySelector(".categories"),modal:document.querySelector(".modal__content"),closeButton:document.querySelector(".modal__close-btn"),backdrop:document.querySelector(".modal"),productsList:document.querySelector(".products")};n.defaults.baseURL="https://dummyjson.com/products/";async function r(){return(await n.get("category-list")).data}function s(e){e.unshift("all");const t=e.map(o=>`<li class="categories__item">
+import"./assets/styles-BK7AYJoX.js";import{a as c}from"./assets/vendor-N5iQpiFS.js";let u=1;const r={categoriesList:document.querySelector(".categories"),modal:document.querySelector(".modal__content"),closeButton:document.querySelector(".modal__close-btn"),backdrop:document.querySelector(".modal"),productsList:document.querySelector(".products")};c.defaults.baseURL="https://dummyjson.com/products";async function p(){return(await c.get("/category-list")).data}async function l(s=1){const o=`?limit=12&skip=${(s-1)*12}`;return(await c.get(o)).data.products}function m(s){const t=s.map(({id:o,title:e,price:n,category:a,thumbnail:i,brand:d})=>`
+      <li class="products__item" data-id="${o}">
+       <img class="products__image" src="${i}" alt="${e}" />
+       <p class="products__title">${e}</p>
+       <p class="products__brand">
+         <span class="products__brand--bold">Brand: ${d}</span>
+       </p>
+       <p class="products__category">Category: ${a} </p>
+       <p class="products__price">Price: ${n}$</p>
+     </li>
+    `).join("");r.productsList.insertAdjacentHTML("beforeend",t)}function _(s){s.unshift("all");const t=s.map(o=>`<li class="categories__item">
         <button class="categories__btn" type="button">${o}</button>
-      </li>`).join("");c.categoriesList.innerHTML=t}async function a(e){try{const t=await r();s(t)}catch(t){console.log(t)}}document.addEventListener("DOMContentLoaded",a);
+      </li>`).join("");r.categoriesList.innerHTML=t}async function g(s){try{const t=await p();_(t);const o=await l(u);m(o)}catch(t){console.log(t)}}document.addEventListener("DOMContentLoaded",g);
 //# sourceMappingURL=index.js.map
